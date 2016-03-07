fa3b639bba664ed79ded155d68f44513441fd452
Ryukie <ryukie.sama@gmail.com>
2016-03-07 18:48:46 +0800

    初步实现倒数开始和停止

---
+++ TimeTodayExtension/TimeTodayExtension/RYTimer.swift
@@ -0,0 +1,105 @@
+//
+//  RYTimer.swift
+//  TimeTodayExtension
+//
+//  Created by 王荣庆 on 16/3/7.
+//  Copyright © 2016年 Ryukie. All rights reserved.
+//
+
+import UIKit
+
+let timerErrorDomain = "SimpleTimerError"
+
+enum SimperTimerError: Int {
+    case AlreadyRunning = 1001
+    case NegativeLeftTime = 1002
+    case NotRunning = 1003
+}
+
+class RYTimer: NSObject {
+    var isRunning : Bool = false
+    
+    var leftTime : NSTimeInterval {
+        didSet {
+            if leftTime < 0 {
+                leftTime = 0
+            }
+        }
+    }
+    // get
+    var leftTimeString : String {
+        get {
+            return leftTime.toString()
+        }
+    }
+
+    private var timerTickHandler: (NSTimeInterval -> ())? = nil
+    private var timerStopHandler: (Bool ->())? = nil
+    private var timer: NSTimer!
+    
+    func start(updateTick: (NSTimeInterval -> Void)?, stopHandler: (Bool -> Void)?) -> (start: Bool, error: NSError?) {
+        
+        if isRunning {
+            return (false,NSError(domain: timerErrorDomain, code: SimperTimerError.NotRunning.rawValue, userInfo: nil))
+        }
+        if leftTime < 0 {
+            return (false,NSError(domain: timerErrorDomain, code: SimperTimerError.NegativeLeftTime.rawValue, userInfo: nil))
+        }
+        timerTickHandler = updateTick
+        timerStopHandler = stopHandler
+        isRunning = true
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector:"countTick", userInfo: nil, repeats: true)
+        return(true,nil)
+    }
+    
+    func stop () -> (stopped:Bool,error:NSError?) {
+        if !isRunning {
+            return (false, NSError(domain: timerErrorDomain, code: SimperTimerError.NotRunning.rawValue, userInfo:nil))
+        }
+        
+        isRunning = false
+        timer.invalidate()
+        timer = nil
+        
+        if let stopHandler = timerStopHandler {
+            stopHandler(leftTime <= 0)
+        }
+        timerStopHandler = nil
+        timerTickHandler = nil
+        
+        return (true,nil)
+    }
+    
+    init(timeInterval:NSTimeInterval) {
+        leftTime = timeInterval
+    }
+    
+    @objc private func countTick() {
+        leftTime -= 1
+        if let tickHandler = timerTickHandler {
+            tickHandler(leftTime)
+        }
+        if leftTime <= 0 {
+            stop()
+        }
+    }
+}
+
+extension NSTimeInterval {
+    func toString() -> String {
+        let totalSecond = Int(self)
+        let minute = totalSecond / 60
+        let second = totalSecond % 60
+        
+        switch (minute, second) {
+        case (0...9, 0...9):
+            return "0\(minute):0\(second)"
+        case (0...9, _):
+            return "0\(minute):\(second)"
+        case (_, 0...9):
+            return "\(minute):0\(second)"
+        default:
+            return "\(minute):\(second)"
+        }
+    }
+}
--- TimeTodayExtension/TimeTodayExtension/ViewController.swift
+++ TimeTodayExtension/TimeTodayExtension/ViewController.swift
@@ -8,8 +8,13 @@
 
 import UIKit
 
+let defaultTimeInterval: NSTimeInterval = 10
+
 class ViewController: UIViewController {
 
+    var timer : RYTimer?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,53 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func bt_startClick(sender: AnyObject) {
+        if timer == nil {
+            timer = RYTimer(timeInterval: defaultTimeInterval)
+        }
+        let (started , error) = timer!.start({ [weak self] (leftTime) -> Void in
+            self!.updateLabel()
+            }, stopHandler: { [weak self] (finished) -> Void in
+                self!.showFinishAlert(finished)
+                self!.timer = nil
+        })
+        //闭包 另一种写法
+//        let (started, error) = timer!.start({
+//            [weak self] leftTick in self!.updateLabel()
+//            }, stopHandler: {
+//                [weak self] finished in
+//                self!.showFinishAlert(finished)
+//                self!.timer = nil
+//            })
+        
+        if started {
+            updateLabel()
+        } else {
+            if let realError = error {
+                print("error: \(realError.code)")
+            }
+        }
+    }
+    @IBAction func bt_stopClick(sender: AnyObject) {
+        if let realTimer = timer {
+            let (stopped, error) = realTimer.stop()
+            if !stopped {
+                if let realError = error {
+                    print("error: \(realError.code)")
+                }
+            }
+        }
+    }
+    private func updateLabel() {
+        lb_leftTime.text = timer!.leftTimeString
+    }
+    private func showFinishAlert(finished: Bool) {
+        let ac = UIAlertController(title: nil , message: finished ? "Finished" : "Stopped", preferredStyle: .Alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .Default, handler: {[weak ac] action in ac!.dismissViewControllerAnimated(true, completion: nil)}))
+        
+        presentViewController(ac, animated: true, completion: nil)
+    }
 
+    @IBOutlet weak var lb_leftTime: UILabel!
 }
 
